c43337bf37a4aaa43fde776fdbb9ec6476e92e77
Your Name <applezafar@gmail.com>
2022-06-24 22:49:18 +1000

    homework cycles

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0) {
+                System.out.println("ping");
+                if (i % 5 == 0) {
+                    System.out.println("pong");
+                } else if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("ping pong");
+                }
+            }
+
+        }
+    }
 }
+
